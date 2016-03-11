728dfd7762b7a633904f785753fc57356e07ba6f
soft <soft05@qq.com>
2016-03-11 15:18:36 +0800

    change file.

--- 多个工厂/src/com/test/factory/SmsSender.java
+++ 多个工厂/src/com/test/factory/SmsSender.java
@@ -1,7 +1,5 @@
 package com.test.factory;
 
-import Sender;
-
 public class SmsSender implements Sender{
 
 	@Override
--- 多个工厂/src/com/test/factory/TestMain.java
+++ 多个工厂/src/com/test/factory/TestMain.java
@@ -1,7 +1,5 @@
 package com.test.factory;
 
-import Sender;
-
 public class TestMain {
 	public static void main(String[] args) {
       ProductFactory factor=new ProductFactory();
