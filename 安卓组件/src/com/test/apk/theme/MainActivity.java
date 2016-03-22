c1f97a82646b9693dd9e177f0b632322d1421ce9
soft <soft05@qq.com>
2016-03-22 20:37:41 +0800

    TestMain

--- 安卓组件/src/com/test/apk/theme/MainActivity.java
+++ 安卓组件/src/com/test/apk/theme/MainActivity.java
@@ -1,18 +1,24 @@
 package com.test.apk.theme;
 
+import java.io.BufferedReader;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
-
+  
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		/*…Ë÷√÷˜Ã‚*/
 		setTheme(android.R.style.Theme_Holo);
 		setContentView(R.layout.activity_main);
+		
+		
 	}
 
+	
 	
 }
